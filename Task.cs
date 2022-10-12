6a65d36c80b2c666684d8e4db76cbbcc4ad46381
hadestorm <spr1ng.h4des@yandex.ru>
2022-10-12 18:01:23 +0100

    Create Task.cs

---
+++ Task.cs
@@ -0,0 +1,13 @@
+﻿namespace Daijou
+{
+    class Task
+    {
+        public Task(int taskID, string taskName)
+        {
+            this.taskID = taskID;
+            this.taskName = taskName;
+        }
+        public int taskID { get; set; }
+        public string taskName { get; set; }
+    }
+}
