6c00eaa54ae3cf9b2cc2583c7d1ac73e784e48db
hadestorm <spr1ng.h4des@yandex.ru>
2022-10-14 20:11:43 +0100

    Update Task.cs

--- Task.cs
+++ Task.cs
@@ -2,12 +2,16 @@
 {
     class Task
     {
-        public Task(int taskID, string taskName)
+        public Task(int taskID, string taskName, string description, DateTime date)
         {
             this.taskID = taskID;
             this.taskName = taskName;
+			this.description = description;
+			this.date = date;
         }
         public int taskID { get; set; }
         public string taskName { get; set; }
+		public string description { get; set; }
+		public DateTime date { get; set; }
     }
 }
